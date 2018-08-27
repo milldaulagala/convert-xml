e95a7d329e5af846ccaea71a220e9a2cff017272
Mill Daulagala <macos@macOSs-MBP.localdomain>
2018-08-27 16:20:19 +1000

    complete Deserialize

---
+++ millxml/millxml/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Xml.Serialization;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+
+
+
+namespace millxml
+{
+
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Xml Serialization
+            Computer obj = new Computer()
+            {
+                name = "Comp_AMS010004",
+                memory = "504",
+                product = "HP d530 SFF(DC578AV)"
+            };
+            XmlSerializer serObj = new XmlSerializer(typeof(Computer));
+            StreamWriter write = new StreamWriter(@"/Users/macos/xmlproject/Computer.xml");
+            serObj.Serialize(write, obj);
+           //Console.WriteLine("Hello World!");
+            write.Close();
+
+            // Xml Deserialize
+            StreamReader rdr = new StreamReader(@"/Users/macos/xmlproject/Computer.xml");
+            Computer DeserializedObj = (Computer)serObj.Deserialize(rdr);
+
+            Console.WriteLine(DeserializedObj.name);
+            Console.WriteLine(DeserializedObj.memory);
+            Console.WriteLine(DeserializedObj.product);
+        }
+    }
+
+
+    public class Computer
+    {
+        public string name { get; set; }
+        public string memory { get; set; }
+        public string product { get; set; }
+    }
+}
